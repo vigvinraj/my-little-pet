567ae875e92f5f76dfc3903032df9717352f235e
light <spot@lights-imac.local>
2016-07-02 17:21:42 -0500

    Initial Commit

---
+++ MyLittlePet/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyLittlePet
+//
+//  Created by light on 7/2/16.
+//  Copyright © 2016 Vignesh Kumar Rajasekaran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
